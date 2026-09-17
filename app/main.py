476bada2bd7dd1b2c20e04a335e423f4972e7702
KaziShoubo <kazishuvo23@gmail.com>
2026-09-17 12:58:08 +0200

    Initial project setup

---
+++ app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Business Analytics API",
+    description="A REST API for business sales management and analytics.",
+    version="1.0.0",
+)
